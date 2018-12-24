db27ac172dd2f67d92a7521f5307e40294c059de
Apoorva <apoorva222g@gmail.com>
2018-12-24 12:11:27 +0530

    ...

--- app/src/main/java/com/woc/apoorva/newsroom/ClubsFragment.java
+++ app/src/main/java/com/woc/apoorva/newsroom/ClubsFragment.java
@@ -1,14 +1,16 @@
 package com.woc.apoorva.newsroom;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ClubsFragment extends Fragment {
+public class ClubsFragment extends Fragment implements TabCult.OnFragmentInteractionListener,TabSports.OnFragmentInteractionListener,TabTech.OnFragmentInteractionListener {
 
     public static ClubsFragment newInstance() {
 
@@ -18,6 +20,12 @@ public class ClubsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_clubs, container, false);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
